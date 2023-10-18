8c60896d0ea7bb5a01073aca595dbbb5e51c859e
darshan1005 <battuladarshan7838@gmail.com>
2023-10-18 12:25:18 +0530

    File of random generator

---
+++ script.js
@@ -0,0 +1,32 @@
+const pswd = document.getElementById('Password');
+const length = 16;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+const lowerCase = "abcefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbols = "@#$%^&*()_+~|}{[]><//-=";
+
+const allChars = upperCase + lowerCase + number + symbols;
+
+function createPSWD(){
+    let password = "";
+
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    pswd.value = password;
+}
+
+function copy(){
+    var copyText = document.getElementById('Password');
+
+    copyText.select();
+    copyText.setSelectionRange(0, 9999);
+
+    navigator.clipboard.writeText(copyText.value);
+}
