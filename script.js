9055c39d5d5bc7004cca48d18ee9a6aacddcb7d1
DarshanBattula <114302987+darshan1005@users.noreply.github.com>
2023-10-18 19:55:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,26 +10,26 @@ const allChars = upperCase + lowerCase + number + symbols;
 
 let length = lengths.value;
 
-lengths.addEventListener('change', (e){
-    length = e.target.value; 
+lengths.addEventListener('change', (e)=>{
+    length = e.target.value;
 });
 
-function createPSWD(){
-    
+function createPSWD() {
+
     let password = "";
 
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password += number[Math.floor(Math.random() * number.length)];
     password += symbols[Math.floor(Math.random() * symbols.length)];
-    while(length > password.length){
+    while (length > password.length) {
         password += allChars[Math.floor(Math.random() * allChars.length)];
     }
 
     pswd.value = password;
 }
 
-function copy(){
+function copy() {
     var copyText = document.getElementById('Password');
 
     copyText.select();
